d3102f71c95c8aafc8473afe112cea77d5f07cf3
argied <jay.arh744@gmail.com>
2022-05-09 11:04:43 +0800

    to be more readable

--- java/src/org/mariuszgromada/math/mxparser/Newton.java
+++ java/src/org/mariuszgromada/math/mxparser/Newton.java
@@ -30,8 +30,15 @@ public class Newton {
 		System.out.printf("\n\n%35s%2s"," ","Continue(any key) or Exit(e)? ");
 		String choice = input.next();
 		switch (choice) {//making choice to whether terminate or continue the operation
-		case "e":System.out.printf("%35s%2s\n"," ","Nice Job!");input.close();break;
-		default:System.out.println("\n\n"); main(null);input.close();break;
+			case "e":
+				System.out.printf("%35s%2s\n"," ","Nice Job!");
+				input.close();
+				break;
+			default:
+				System.out.println("\n\n"); 
+				main(null);
+				input.close();
+				break;
 		}
 	}	
 
@@ -62,35 +69,39 @@ public class Newton {
 		System.out.printf("\n%35s%2s\n"," ","<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>" );
 		System.out.printf("%50s%11s%15s%15s%15s%15s\n", "Iteration", "x0", "x1", "f(x0)", "f'(x0)","Ea");
 		System.out.printf("%35s%2s\n"," ","<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>" );
-
+		
+		double RfOrig = round(fOrig.calculate(), rounding);
+		double RfPrime = round(fPrime.calculate(),rounding);
+		
 		double x0 = Double.parseDouble(valueX);
-		double x1 =  x0 - ((round(fOrig.calculate(), rounding)/round(fPrime.calculate(),rounding)));
-		double Ea = Math.abs(x1 - x0);
+		double Rx0 = round(x0, rounding);
+		
+		double x1 =  Rx0 - (RfOrig/RfPrime);
+		double Rx1 = round(x1, rounding);
+		
+		double Ea = Math.abs(Rx1 - Rx0);
 
 		System.out.printf("%46d%15s%15s%15s%15s\n", 1, df.format(x0), df.format(x1), df.format(fOrig.calculate()), df.format(fPrime.calculate()));
-
 		int i =2;
 		while (Ea>z) {//looping the iterations from 2 to nth
-			String newX1 = String.valueOf(round(x1, rounding));
-			Argument test1 = new Argument ("x = "+newX1);
-
+			Argument test1 = new Argument ("x = "+String.valueOf(Rx1));
 			fOrig = new Expression ("f(x)", f, test1);
-			fPrime = new Expression ("der("+func+", x, "+newX1+")");
+			fPrime = new Expression ("der("+func+", x, "+String.valueOf(Rx1)+")");
+			
+			RfOrig = round(fOrig.calculate(), rounding);
+			RfPrime = round(fPrime.calculate(),rounding);
 
 			x0=x1;
-			x1 =  round(x0, rounding) - ((round(fOrig.calculate(), rounding)/round(fPrime.calculate(),rounding)));
-			Ea = Math.abs(round(x1, rounding) - round(x0, rounding));
+			Rx0 = round(x0, rounding);
+			x1 =  Rx0 - (RfOrig/RfPrime);
+			Rx1 = round(x1, rounding);
+			Ea = Math.abs(Rx1 - Rx0);
 
 			System.out.printf("%46d%15s%15s%15s%15s%15s\n", i, df.format(x0), df.format(x1), df.format(fOrig.calculate()), df.format(fPrime.calculate()), df.format(Ea));
-
 			i++;            
-			if (i>100) {
-				System.out.printf("%35s%2s\n"," ","Seems to be infinite");
-				break;
-			}
+			if (i>100) {System.out.printf("%35s%2s\n"," ","Seems to be infinite");break;}
 			if (Ea ==0)break;
 		}
-
 		System.out.printf("%35s%2s\n"," ","<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>" );
 		System.out.printf("\n%35s%2s\n"," ","At iteration #"+(i-1)+", the root is approximately "+df.format(x1));//displaying the approximate value of x
 
@@ -101,8 +112,7 @@ public class Newton {
 	static double round(double value, int places) {
 		if (places < 0) 
 			throw new IllegalArgumentException();
-		BigDecimal bd = new BigDecimal(Double.toString(value));
-		
+		BigDecimal bd = new BigDecimal(Double.toString(value));		
 		bd = bd.setScale(places, RoundingMode.HALF_UP);
 		return bd.doubleValue();
 	}
